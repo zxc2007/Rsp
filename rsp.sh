b2ff53d297e4b07a89e91db7aa179869a5005e4e
●┼──◡̈⃝   ˢʰᵃᵈᵒʷ••✓⁩ <66058079+evildevill@users.noreply.github.com>
2020-09-29 18:46:03 +0500

    Rsp BaSh file installer

---
+++ rsp.sh
@@ -0,0 +1,47 @@
+pip install lolcat
+echo '
+       __Hey guys :)
+   _  |@@|
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "\...Script By { HaCkEr WaSii }.../"| lolcat
+echo " "
+apt-get update -y
+apt-get upgrade -y
+pkg install nano -y
+pkg install git -y
+pkg install perl -y
+pkg install python -y
+pkg install python2 -y
+pip install --upgrade pip 
+git clone https://github.com/threat9/routersploit
+cd routersploit
+pkg install clang -y
+apt install libffi libffi-dev -y
+pkg install python-dev -y clang libcrypt-dev -y libffi-dev -y git openssl-dev -y && export CONFIG_SHELL=$PREFIX/bin/sh
+pkg upgrade && pkg install autoconf -y automake -y bison -y bzip2 -y clang -y cmake -y \ coreutils -y diffutils -y flex -y gawk -y git -y grep -y gzip -y libtool -y make -y patch -y perl -y \ sed -y 
+tar -y wget -y pkg-config -y silversearcher-ag -y
+pip install future
+termux-fix-shebang rsf.py
+pip install cryptography
+pip install requests
+pip install paramiko
+pip install pysnmp
+pip install pycrypto
+pip install pycryptodome
+pip install pysnmp==4.4.6
+pip install pytest==3.6.0
+pip install pytest-forked
+pip install pytest-xdist
+pip install flake8
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+echo " "
+echo "###### Now use the tool ######" | lolcat
+python3 rsf.py
